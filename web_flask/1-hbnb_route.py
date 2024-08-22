f8aba0d522b5bcd4e11fd0db592d82e0da781c9c
ElusiveJames <86646115+ElusiveJames@users.noreply.github.com>
2024-08-23 00:00:33 +0300

    hbnb

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" creating a flask app """
+from flask import Flask
+""" class Flask """
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """ Return Hello HBNB! """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """ Return HBNB """
+    return "HBNB"
+
+
+if __name__ == "__main__":
+    """ Main Function """
+    app.run(host='0.0.0.0', port=5000)
